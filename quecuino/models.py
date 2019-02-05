d21208ffd1b6e0f999084f8964fb7bcf7e814772
saiko <pferngonz@gmail.com>
2019-02-05 19:29:46 +0100

    versio 0.1 - Pablo Fernández

--- quecuino/models.py
+++ quecuino/models.py
@@ -4,14 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Recepta(models.Model):
-    nom_recepta = models.CharField(max_length=16)
-    data_creacio = models.DateField()
-    descripcio = models.CharField(max_length=100)
-    procediment = models.CharField(max_length=2000)
-    vots = models.IntegerField()
-    imatge = models.CharField(max_length=50, default='')
-
 class Usuari(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nom = models.CharField(max_length=15)
@@ -22,5 +14,15 @@ class Usuari(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Recepta(models.Model):
+    nom_recepta = models.CharField(max_length=16)
+    user = models.ForeignKey(Usuari, on_delete=models.CASCADE)
+    data_creacio = models.DateField()
+    descripcio = models.CharField(max_length=100)
+    procediment = models.CharField(max_length=2000)
+    vots = models.IntegerField()
+    imatge = models.CharField(max_length=50, default='')
+
 
 
