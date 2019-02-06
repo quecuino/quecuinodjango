a19b79e5128ffa375b97dce6de371a05f2924dc0
saiko <pferngonz@gmail.com>
2019-02-06 18:43:36 +0100

    versio 0.4 - Pablo Fernández

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.modal').modal();
     $('.collapsible').collapsible();
     $('.fixed-action-btn').floatingActionButton();
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
 });
